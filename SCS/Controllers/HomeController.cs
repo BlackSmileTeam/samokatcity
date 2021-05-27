702e2d9c7722293951512d59728e1f0f04f52257
Владислав Секисов <vasekisov@gmail.com>
2021-05-28 00:13:42 +0300

    Added profit withdrawal for yesterday

--- SCS/Controllers/HomeController.cs
+++ SCS/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace SCS.Controllers
 {
@@ -12,11 +13,41 @@ namespace SCS.Controllers
 		SCSContext db = new SCSContext();
 		public ActionResult Index()
 		{
+			decimal YesterdayProfit = 0;
+			decimal YesterdayCashPaymentProfit = 0;
+			decimal YesterdayCardPaymentProfit = 0;
+			decimal YesterdayBonusPaymentProfit = 0;
+
+
 			ViewData["TransportModels"] = db.TransportModels.ToList();
 			ViewData["FreeTransport"] = db.Transport.SqlQuery("CALL transport_vw('" + DateTime.Now.ToString("yyyy-MM-dd HH:mm") + "')").ToList().Count;
 			ViewData["FreeAccessories"] = db.Accessories.SqlQuery("CALL accessories_vw('" + DateTime.Now.ToString("yyyy-MM-dd HH:mm") + "')").ToList().Count;
 			var users = db.Users;
 			ViewBag.User = users;
+
+			var dateYesterday = DateTime.Today.AddDays(-1);
+
+			var orders = db.Orders.Include(p => p.Payment).Include(t => t.OrderTransports.Select(r => r.Rates)).Where(o => o.DateStart >= dateYesterday).ToList();
+			foreach (var order in orders)
+			{
+				if (order.OrderTransports != null)
+				{
+					//Добавляем на всякий случай еще час
+					var dateEndDuration = DateTime.Today.AddHours(order.OrderTransports.First().Rates.Duration + 1);
+					if (order.DateEnd <= dateEndDuration)
+					{
+						var payment = order.Payment;
+						YesterdayProfit += payment.CashDeposit + payment.CashPayment + payment.CardDeposit + payment.CardPayment;
+						YesterdayCashPaymentProfit += payment.CashDeposit + payment.CashPayment;
+						YesterdayCardPaymentProfit += payment.CardDeposit + payment.CardPayment;
+						YesterdayBonusPaymentProfit += payment.BonusPayment;
+					}
+				}
+			}
+			ViewData["YesterdayProfit"] = YesterdayProfit;
+			ViewData["YesterdayCashPaymentProfit"] = YesterdayCashPaymentProfit;
+			ViewData["YesterdayCardPaymentProfit"] = YesterdayCardPaymentProfit;
+			ViewData["YesterdayBonusPaymentProfit"] = YesterdayBonusPaymentProfit;
 
 			return View();
 		}
