f0d2dc5b6b05c6e6f2a7c31129d90bae117e1e95
Vladislav Sekisov <Vladislav.Sekisov@psi-cro.com>
2021-06-23 20:51:52 +0300

    fixed deleting users and displaying a notification when deleting

--- SCS/Controllers/UsersController.cs
+++ SCS/Controllers/UsersController.cs
@@ -11,197 +11,211 @@ using SCS.Models;
 
 namespace SCS.Controllers
 {
-	public class UsersController : Controller
-	{
-		private SCSContext db = new SCSContext();
+    public class UsersController : Controller
+    {
+        private SCSContext db = new SCSContext();
 
-		// GET: Users
-		public async Task<ActionResult> Index()
-		{
-			var users = db.Users.Include(u => u.ContactUser);
-			return View(await users.ToListAsync());
-		}
+        // GET: Users
+        public async Task<ActionResult> Index()
+        {
+            var users = db.Users.Include(u => u.ContactUser);
+            return View(await users.ToListAsync());
+        }
 
-		// GET: Users/Details/5
-		public ActionResult Details(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
-			User user = db.Users.Include(cu => cu.ContactUser).FirstOrDefault(u => u.Id == id);
-			if (user == null)
-			{
-				return HttpNotFound();
-			}
-			//user.ContactUser = db.ContactUser.Find(user.ContactUserId);
-
-
-			return PartialView(user);
-		}
-
-		// GET: Users/Create
-		public ActionResult Create()
-		{
-			return View();
-		}
-
-		// POST: Users/Create
-		// Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. 
-		// Дополнительные сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<ActionResult> Create(string Username, string Password, string Passport, string City,
-											   string Home, string Apartament, string Surname, string Name, string Street,
-											   string Patronymic, string Phone)
-		{
-			User user = new User();
-			if (ModelState.IsValid)
-			{
-				user = new User()
-				{
-					Username = Username,
-					Password = Password
-				};
-				var userInitialsName = (Name != null && Name.Length > 0) ? Name[0] : ' ';
-				var userInitialsPatronymic = (Patronymic != null && Patronymic.Length > 0) ? Patronymic[0] : ' ';
-				СontactUser contactUser = new СontactUser()
-				{
-					Passport = Passport,
-					City = City,
-					Home = Home,
-					Apartment = Apartament,
-					Surname = Surname,
-					Name = Name,
-					Patronymic = Patronymic,
-					Phone = Phone,
-					Street = Street,
-					ShortName = $"{Surname} {userInitialsName}. {userInitialsPatronymic}."
-				};
-				db.ContactUser.Add(contactUser);
-				await db.SaveChangesAsync();
-
-				//Добавляем созданные данные пользователя к авторизационным данным
-				user.ContactUser = contactUser;
-				//У полученные данных пользователя и сохраненных в БД берем ID (которые сгенерирован автоматически) и присваиваем в модель пользователя
-				//user.ContactUserId = contactUser.Id;
-
-				db.Users.Add(user);
-				await db.SaveChangesAsync();
-				return RedirectToAction("Index");
-			}
-
-			//ViewBag.ContactUserId = new SelectList(db.ContactUser, "Id", "ShortName", user.ContactUserId);
-			return View(user);
-		}
-
-		// GET: Users/Edit/5
-		public async Task<ActionResult> Edit(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
-			User user = await db.Users.Include(c => c.ContactUser).FirstOrDefaultAsync(u => u.Id == id);
-			if (user == null)
-			{
-				return HttpNotFound();
-			}
-			return View(user);
-		}
-
-		// POST: Users/Edit/5
-		// Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. 
-		// Дополнительные сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<ActionResult> Edit(int Id, string Username, string Password, string Passport, string City,
-											   string Home, string Apartment, string Surname, string Street,
-											   string Patronymic, string Phone, string Name)
-		{
-			User user = db.Users.Include(cu => cu.ContactUser).FirstOrDefault(u => u.Id == Id);
-			if (user != null)
-			{
-				user.Username = Username;
-				user.Password = Password;
-				var userInitialsName = (Name != null && Name.Length > 0) ? Name[0] : ' ';
-				var userInitialsPatronymic = (Patronymic != null && Patronymic.Length > 0) ? Patronymic[0] : ' ';
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Include(cu => cu.ContactUser).FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            //user.ContactUser = db.ContactUser.Find(user.ContactUserId);
 
 
-				user.ContactUser.Passport = Passport;
-				user.ContactUser.City = City;
-				user.ContactUser.Home = Home;
-				user.ContactUser.Apartment = Apartment;
-				user.ContactUser.Surname = Surname;
-				user.ContactUser.Name = Name;
-				user.ContactUser.Patronymic = Patronymic;
-				user.ContactUser.Phone = Phone;
-				user.ContactUser.Street = Street;
-				user.ContactUser.ShortName = $"{Surname} {userInitialsName}. {userInitialsPatronymic}.";
+            return PartialView(user);
+        }
 
-				if (ModelState.IsValid)
-				{
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
 
-					db.Entry(user).State = EntityState.Modified;
-					//db.Entry(сontactUser).State = EntityState.Modified;
-					await db.SaveChangesAsync();
-					return RedirectToAction("Index");
-				}
-			}
-			return View(user);
-		}
+        // POST: Users/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. 
+        // Дополнительные сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(string Username, string Password, string Passport, string City,
+                                               string Home, string Apartament, string Surname, string Name, string Street,
+                                               string Patronymic, string Phone)
+        {
+            User user = new User();
+            if (ModelState.IsValid)
+            {
+                user = new User()
+                {
+                    Username = Username,
+                    Password = Password
+                };
+                var userInitialsName = (Name != null && Name.Length > 0) ? Name[0] : ' ';
+                var userInitialsPatronymic = (Patronymic != null && Patronymic.Length > 0) ? Patronymic[0] : ' ';
+                СontactUser contactUser = new СontactUser()
+                {
+                    Passport = Passport,
+                    City = City,
+                    Home = Home,
+                    Apartment = Apartament,
+                    Surname = Surname,
+                    Name = Name,
+                    Patronymic = Patronymic,
+                    Phone = Phone,
+                    Street = Street,
+                    ShortName = $"{Surname} {userInitialsName}. {userInitialsPatronymic}."
+                };
+                db.ContactUser.Add(contactUser);
+                await db.SaveChangesAsync();
 
-		// GET: Users/Delete/5
-		public async Task<ActionResult> Delete(int? id)
-		{
-			if (id == null)
-			{
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-			}
-			User user = await db.Users.Include(cu => cu.ContactUser).FirstOrDefaultAsync(u => u.Id == id);
-			if (user == null)
-			{
-				return HttpNotFound();
-			}
-			return View(user);
-		}
+                //Добавляем созданные данные пользователя к авторизационным данным
+                user.ContactUser = contactUser;
+                //У полученные данных пользователя и сохраненных в БД берем ID (которые сгенерирован автоматически) и присваиваем в модель пользователя
+                //user.ContactUserId = contactUser.Id;
 
-		// POST: Users/Delete/5
-		[HttpPost, ActionName("Delete")]
-		[ValidateAntiForgeryToken]
-		public async Task<ActionResult> DeleteConfirmed(int id)
-		{
-			User user = await db.Users.FindAsync(id);
-			//СontactUser сontactUser = await db.ContactUser.FindAsync(user.ContactUserId);
+                db.Users.Add(user);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
 
-			//foreach (var order in db.Orders.Include(p => p.Payment).Include(oa => oa.OrderAccessories).Include(ot => ot.OrderTransports).Where(u => u.UserId == id))
-			//{
-			//	foreach (var OA in order.OrderAccessories.ToList())
-			//	{
-			//		db.OrderAccessories.Remove(OA);
-			//	}
-			//	foreach (var OT in order.OrderTransports.ToList())
-			//	{
-			//		db.OrderTransport.Remove(OT);
-			//	}
+            //ViewBag.ContactUserId = new SelectList(db.ContactUser, "Id", "ShortName", user.ContactUserId);
+            return View(user);
+        }
 
-			//	db.Payment.Remove(order.Payment);
-			//	db.Orders.Remove(order);
-			//}
+        // GET: Users/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = await db.Users.Include(c => c.ContactUser).FirstOrDefaultAsync(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
 
-			db.Users.Remove(user);
-			//db.ContactUser.Remove(сontactUser);
-			await db.SaveChangesAsync();
-			return RedirectToAction("Index");
-		}
+        // POST: Users/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. 
+        // Дополнительные сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int Id, string Username, string Password, string Passport, string City,
+                                               string Home, string Apartment, string Surname, string Street,
+                                               string Patronymic, string Phone, string Name)
+        {
+            User user = db.Users.Include(cu => cu.ContactUser).FirstOrDefault(u => u.Id == Id);
+            if (user != null)
+            {
+                user.Username = Username;
+                user.Password = Password;
+                var userInitialsName = (Name != null && Name.Length > 0) ? Name[0] : ' ';
+                var userInitialsPatronymic = (Patronymic != null && Patronymic.Length > 0) ? Patronymic[0] : ' ';
 
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing)
-			{
-				db.Dispose();
-			}
-			base.Dispose(disposing);
-		}
-	}
+
+                user.ContactUser.Passport = Passport;
+                user.ContactUser.City = City;
+                user.ContactUser.Home = Home;
+                user.ContactUser.Apartment = Apartment;
+                user.ContactUser.Surname = Surname;
+                user.ContactUser.Name = Name;
+                user.ContactUser.Patronymic = Patronymic;
+                user.ContactUser.Phone = Phone;
+                user.ContactUser.Street = Street;
+                user.ContactUser.ShortName = $"{Surname} {userInitialsName}. {userInitialsPatronymic}.";
+
+                if (ModelState.IsValid)
+                {
+
+                    db.Entry(user).State = EntityState.Modified;
+                    //db.Entry(сontactUser).State = EntityState.Modified;
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = await db.Users.Include(cu => cu.ContactUser).FirstOrDefaultAsync(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            User user = await db.Users.FindAsync(id);
+
+            var orders = db.Orders.Include(u => u.User).Include(p => p.Payment).Where(o => o.User.Id == id);
+
+            List<OrderAccessories> orderAccessories = db.OrderAccessories.Include(o => o.Order).ToList();
+            List<OrderTransport> orderTransports = db.OrderTransport.Include(o => o.Order).ToList();
+            foreach (var order in orders)
+            {
+                orderAccessories = orderAccessories.Where(oa => oa.Order.Id == order.Id).ToList();
+                orderTransports = orderTransports.Where(oa => oa.Order.Id == order.Id).ToList();
+
+                foreach (var oa in orderAccessories)
+                {
+                    db.OrderAccessories.Remove(oa);
+                }
+                foreach (var ot in orderTransports)
+                {
+                    db.OrderTransport.Remove(ot);
+                }
+
+                db.Orders.Remove(order);
+
+                if (order.Payment != null)
+                {
+                    user.Bonus -= order.AddBonuses + order.Payment.BonusPayment;
+                    db.Entry(user).State = EntityState.Modified;
+                    db.Payment.Remove(order.Payment);
+                }
+            }
+
+            await db.SaveChangesAsync();
+
+            db.Users.Remove(user);
+
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
 }
